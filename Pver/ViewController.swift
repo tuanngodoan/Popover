ec51879fdc580bb19d09729493a70448190cdbb6
Doãn Tuấn <tuanngodoan@hotmail.com>
2016-11-18 11:20:38 +0700

    Show Popover and design UI FB

--- Pver/ViewController.swift
+++ Pver/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPopoverPresentationControllerDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        //segue for the popover configuration window
+        if segue.identifier == "showOver" {
+            let controller = segue.destination
+            controller.popoverPresentationController!.delegate = self
+            controller.popoverPresentationController?.backgroundColor = UIColor.white
+            controller.preferredContentSize = CGSize(width: 220, height: 30)
+            
+        }
+    }
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle
+    {
+        return .none
+    }
+    
 }
 
